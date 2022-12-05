c1325b776ceef60aab4c4304cc77d21d4404dea3
raphactn <raphael.d.caetano48@gmail.com>
2022-12-05 20:35:13 -0300

    ajuste no chrome

--- src/services/ListResultsServices.ts
+++ src/services/ListResultsServices.ts
@@ -1,8 +1,19 @@
-import puppeteer from "puppeteer";
+let chrome: any = {};
+let puppeteer: any;
+
+chrome = require("chrome-aws-lambda");
+puppeteer = require("puppeteer-core");
 
 const ListResultsServices = async ({ data, type }: any) => {
- 
-  let browser = await puppeteer.launch({headless: true});
+  const options = {
+    args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+    defaultViewport: chrome.defaultViewport,
+    executablePath: await chrome.executablePath,
+    headless: true,
+    ignoreHTTPSErrors: true,
+  };
+
+  let browser = await puppeteer.launch(options);
 
   let error = false;
   let typeMedia = type;
