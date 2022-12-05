4b437ca531166ffef8e243b884cd4a681cb791f5
raphactn <raphael.d.caetano48@gmail.com>
2022-12-04 22:42:45 -0300

    teste puppeter correção

--- services/ListResultsServices.ts
+++ services/ListResultsServices.ts
@@ -1,12 +1,12 @@
-import chromium from "chrome-aws-lambda";
 
 const ListResultsServices = async ({ data, type }: any) => {
+    const chrome = require('chrome-aws-lambda');
 
     let error = false
     let typeMedia = type
     let result = []
 
-    const browser = await chromium.puppeteer.launch({ headless: true });
+    const browser = await chrome.puppeteer.launch({ headless: true });
     const page = await browser.newPage();
     page.setCacheEnabled(false)
     await page.goto(data as string);
@@ -14,14 +14,14 @@ const ListResultsServices = async ({ data, type }: any) => {
     if (type === 'image') {
         await page.waitForSelector("._aagv").then(() => {
             return error = false
-        }).catch((err) => {
+        }).catch(() => {
             return error = true
         })
         result = await page.$$eval("._aagv > img", (images: any) => images.map((i: any) => i.src));
     } else {
         await page.waitForSelector("._ab1c").then(() => {
             return error = false
-        }).catch((err) => {
+        }).catch(() => {
             return error = true
         })
         result = await page.$$eval("._ab1c > video", (images: any) => images.map((i: any) => i.src));
