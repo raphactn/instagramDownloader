0a6304aa69090ed7c84f6b7371c4889c7c9367fc
raphactn <raphael.d.caetano48@gmail.com>
2022-12-04 22:50:00 -0300

    correção puppeter para vercel

--- services/ListResultsServices.ts
+++ services/ListResultsServices.ts
@@ -1,35 +1,58 @@
-
 const ListResultsServices = async ({ data, type }: any) => {
-    const chrome = require('chrome-aws-lambda');
+  let chrome = {};
+  let puppeteer;
 
-    let error = false
-    let typeMedia = type
-    let result = []
+  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+    // running on the Vercel platform.
+    chrome = require("chrome-aws-lambda");
+    puppeteer = require("puppeteer-core");
+  } else {
+    // running locally.
+    puppeteer = require("puppeteer");
+  }
 
-    const browser = await chrome.puppeteer.launch({ headless: true });
-    const page = await browser.newPage();
-    page.setCacheEnabled(false)
-    await page.goto(data as string);
+  let browser = await puppeteer.launch({
+    headless: true,
+    ignoreHTTPSErrors: true,
+  });
 
-    if (type === 'image') {
-        await page.waitForSelector("._aagv").then(() => {
-            return error = false
-        }).catch(() => {
-            return error = true
-        })
-        result = await page.$$eval("._aagv > img", (images: any) => images.map((i: any) => i.src));
-    } else {
-        await page.waitForSelector("._ab1c").then(() => {
-            return error = false
-        }).catch(() => {
-            return error = true
-        })
-        result = await page.$$eval("._ab1c > video", (images: any) => images.map((i: any) => i.src));
-    }
+  let error = false;
+  let typeMedia = type;
+  let result = [];
 
-    await browser.close();
+  const page = await browser.newPage();
+  page.setCacheEnabled(false);
+  await page.goto(data as string);
 
-    return { result, error, typeMedia }
+  if (type === "image") {
+    await page
+      .waitForSelector("._aagv")
+      .then(() => {
+        return (error = false);
+      })
+      .catch(() => {
+        return (error = true);
+      });
+    result = await page.$$eval("._aagv > img", (images: any) =>
+      images.map((i: any) => i.src)
+    );
+  } else {
+    await page
+      .waitForSelector("._ab1c")
+      .then(() => {
+        return (error = false);
+      })
+      .catch(() => {
+        return (error = true);
+      });
+    result = await page.$$eval("._ab1c > video", (images: any) =>
+      images.map((i: any) => i.src)
+    );
+  }
+
+  await browser.close();
+
+  return { result, error, typeMedia };
 };
 
-export { ListResultsServices }
+export { ListResultsServices };
