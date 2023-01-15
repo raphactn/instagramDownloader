1f9e50e88b471864afee4dbd82497da5a499993a
raphactn <raphael@leeksolucoes.com.br>
2023-01-15 19:53:17 -0300

    ajustes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,61 +6,34 @@ import {
   SimpleGrid,
   Container,
   Image,
-  Spinner,
   Box,
-  useToast,
-  Tabs,
-  TabList,
-  TabPanels,
-  Tab,
-  TabPanel,
 } from "@chakra-ui/react";
-import { GetServerSideProps } from "next";
 import Head from "next/head";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
-import { ListResultsServices } from "../services/ListResultsServices";
+import { useState } from "react";
 import Nav from "../components/Navbar";
+import api from "./api/main";
 
-export default function Home({ data, error, typeMedia }: any) {
+export default function Home() {
   const [url, setUrl] = useState("");
-  const [urlVideo, setUrlVideo] = useState("");
-  const [loadingPage, setLoadingPage] = useState(false);
   const [dataPage, setDataPage] = useState([]);
-  const router = useRouter();
-  const toast: any = useToast();
 
-  const handleGetMedia = () => {
-    if (url !== "" || urlVideo !== "") {
-      setLoadingPage(true);
-      setDataPage([]);
-      router.replace({
-        query: {
-          url: url ? url : urlVideo,
-          type: url ? "image" : "video",
-        },
-      });
-      setUrl("");
-      setUrlVideo("");
+  const fetchData = () => {
+    if (!url) {
+      return;
     }
+    api
+      .post("/instagramData", {
+        body: url,
+        headers: { "Content-Type": "application/json" },
+      })
+      .then((res) => {
+        setDataPage(res.data.result);
+        setUrl("")
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
-
-  useEffect(() => {
-    if (error) {
-      return toast({
-        title: "Erro ao trazer media, tente novamente mais tarde :(",
-        status: "error",
-        isClosable: true,
-      });
-    }
-  }, [error]);
-
-  useEffect(() => {
-    if (data) {
-      setLoadingPage(false);
-      setDataPage(data);
-    }
-  }, [data]);
 
   const downloadImage = (url: string, name: string) => {
     fetch(url)
@@ -86,106 +59,48 @@ export default function Home({ data, error, typeMedia }: any) {
       </Head>
       <Nav />
       <Container maxW="1000px">
-        <Tabs variant="enclosed" mt={10}>
-          <TabList justifyContent={"center"}>
-            <Tab>Imagem</Tab>
-            <Tab>Video</Tab>
-          </TabList>
-          <TabPanels>
-            <TabPanel>
-              <Center gap={2}>
-                <Input
-                  value={url}
-                  placeholder="Adicione a URL da imagem aqui ou link do perfil"
-                  onChange={(e) => setUrl(e.target.value)}
-                />
-                <Button variant={"solid"} onClick={handleGetMedia}>
-                  Buscar
-                </Button>
-              </Center>
-            </TabPanel>
-            <TabPanel>
-              <Center gap={2}>
-                <Input
-                  value={urlVideo}
-                  placeholder="Adicione a URL do video"
-                  onChange={(e) => setUrlVideo(e.target.value)}
-                />
-                <Button variant={"solid"} onClick={handleGetMedia}>
-                  Buscar
-                </Button>
-              </Center>
-            </TabPanel>
-          </TabPanels>
-        </Tabs>
+        <Center gap={2} mt={5}>
+          <Input
+            value={url}
+            placeholder="Adicione a URL da imagem aqui ou link do perfil"
+            onChange={(e) => setUrl(e.target.value)}
+          />
+          <Button variant={"solid"} onClick={fetchData}>
+            Buscar
+          </Button>
+        </Center>
         <Center mt={50} flexDirection="column" gap={2}>
           {dataPage ? (
             <SimpleGrid columns={{ base: 1, md: 4 }} spacing={10} mb={10}>
               {dataPage?.map((item: string, i: number) => (
                 <Box key={i}>
                   <Flex direction="column">
-                    {typeMedia === "image" ? (
-                      <Image
-                        borderTopEndRadius={"md"}
-                        borderTopStartRadius={"md"}
-                        crossOrigin="anonymous"
-                        src={`https://cors-anywhere.herokuapp.com/${item}`}
-                        width={400}
-                      />
-                    ) : (
-                      <video
-                        crossOrigin="anonymous"
-                        controls={true}
-                        autoPlay={false}
-                      >
-                        <source
-                          src={`https://cors-anywhere.herokuapp.com/${item}`}
-                          type="video/mp4"
-                        />
-                      </video>
-                    )}
+                    <Image
+                      borderTopEndRadius={"md"}
+                      borderTopStartRadius={"md"}
+                      crossOrigin="anonymous"
+                      src={`https://cors-anywhere.herokuapp.com/${item}`}
+                      width={400}
+                    />
                     <Button
                       borderTopEndRadius={0}
                       borderTopStartRadius={0}
                       onClick={() =>
                         downloadImage(
                           `https://cors-anywhere.herokuapp.com/${item}`,
-                          `media.${typeMedia === "image" ? "png" : "mp4"}`
+                          `media.png`
                         )
                       }
                     >
-                      {typeMedia === "image" ? "Baixar imagem" : "Baixar video"}
+                      Baixar imagem
                     </Button>
                   </Flex>
                 </Box>
               ))}
             </SimpleGrid>
           ) : null}
-          {loadingPage ? <Spinner /> : null}
         </Center>
       </Container>
     </div>
   );
 }
-
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const data = ctx.query.url ? ctx.query.url : "";
-  const type = ctx.query.type ? ctx.query.type : "";
-
-  if (data !== "") {
-    const { result, error, typeMedia } = await ListResultsServices({
-      data,
-      type,
-    });
-    return {
-      props: {
-        data: result,
-        error,
-        typeMedia,
-      },
-    };
-  }
-  return {
-    props: {},
-  };
-};
