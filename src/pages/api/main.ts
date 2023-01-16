4bd4a2624f148fb58cbe1c9b8604ee221efc7489
raphactn <raphael@leeksolucoes.com.br>
2023-01-16 09:14:49 -0300

    ajuste remove cors

--- src/pages/api/main.ts
+++ src/pages/api/main.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://cors-anywhere.herokuapp.com/https://instagramserverapi.herokuapp.com",
+  baseURL: "https://instagramserverapi.herokuapp.com",
 });
 
 export default api;
